754209cb20898239860f7a2d20eb110d6366e791
root <root@FWD_YF_145_202.(none)>
2016-09-01 16:55:53 +0800

    build the structure about the project.

---
+++ ebk2totxt.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class BookInfo
+{
+
+
+};
+
+
+class BookItems
+{
+
+
+};
+
+
+class BytesToStruct
+{
+
+
+};
+
+
+class CATHYEBK_MEDIA_DATA_T
+{
+
+};
+
+
+class ChapterCompress
+{
+
+
+};
+
+
+class ChapterStruct
+{
+
+};
+
+
+class Ebk2Open
+{
+
+
+};
+
+
+class Model
+{
+
+
+};
+
+
+class TextStruct
+{
+
+
+};
+
+
+int main(int argc, char * argv[])
+{
+
+
+    return 0;
+}
+
+

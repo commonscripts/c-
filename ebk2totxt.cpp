8448040d0f7af45619a07ef18bf831ebd3c1efa3
root <root@FWD_YF_145_202.(none)>
2016-09-01 17:01:30 +0800

    finish the class TextStruct.

--- ebk2totxt.cpp
+++ ebk2totxt.cpp
@@ -60,8 +60,20 @@ class Model
 
 class TextStruct
 {
-
-
+    public:
+        int book_id;
+        int head_data_size;
+        int ebk_version;
+        int ebk_size;
+        char book_name[64];
+        int file_size;
+        int head_compress_size;
+        int first_compress_block_size;
+        int chapter_count;
+        int compress_block_count;
+        int media_count;
+        int media_data_length;
+        int txt_compress_size;
 };
 
 
